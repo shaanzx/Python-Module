66e90b8bbfab6e4ea1a66c78f88fa22c37aaccf1
Shaanzx <anuhaslakshan07@gmail.com>
2025-07-02 14:58:59 +0530

    Python package done

---
+++ Day 05/python package/main.py
@@ -0,0 +1,10 @@
+# from My_Calculator import subtract
+# from My_Calculator import add
+import My_Calculator
+
+result = My_Calculator.add(2 , 3)
+print(f"Sum is : {result}")
+
+result = My_Calculator.subtract(5 , 3)
+print(f"Different is : {result}")
+
---
+++ Day 05/python package/my_calculator/__init__.py
@@ -0,0 +1,4 @@
+from .addition import add
+from .subtraction import subtract
+
+print("My calculator package has been initialized...")
