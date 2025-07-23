1d1026b06235a14ba30f7864c3230dca202aa0cd
Shaanzx <anuhaslakshan07@gmail.com>
2025-07-23 16:42:39 +0530

    Python Module done

---
+++ Day 08/05_BreakPoint.py
@@ -0,0 +1,9 @@
+def multification(a, b):
+    return a * b
+
+breakpoint()
+x = input("Enter a number: ")
+y = input("Enter another number: ")
+
+result = multification(x, y)
+print(f"{x} * {y} = {result}")
